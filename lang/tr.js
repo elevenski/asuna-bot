27327e3dab011b264220e0c04f2cc65afd1b81ce
Eleven <alperiince0@gmail.com>
2021-02-28 14:14:15 +0300

    Create tr.js

---
+++ lang/tr.js
@@ -0,0 +1,86 @@
+module.exports = {
+
+    embed: {
+    //
+    },
+    
+    stats: {
+    guilds: "Sunucular",
+    users: "Kullanıcılar",
+    channels: "Kanallar",
+    developers: "Geliştiriciler"
+    },
+    
+    nsfw: {
+    votereq: "NSFW Komutlarını kullanmak için, **Top.gg** üzerinden bota 12 saatte 1 oy vermelisiniz.",
+    onreq: "NSFW Komutlarını kullanmak için lütfen kanalın NSFW özelliğini açın."    
+    },
+    
+    eightball: {
+     questionreq: "Lütfen bir soru girin. `+8ball soru`",
+     answers_one: "Evet!",
+     answers_two: "Hayır...",
+     answers_three: "Ne?",
+     answers_four: "Ciddi?",
+     answers_five: "Buna cevap vermek istemiyorum.",
+     answers_six: "Asla!",
+     answers_seven: "Tekrar deneyim...",
+     answers_eight: "Kendini geliştirmelisin.",
+     answers_nine: "LÜTFEN BU SORUYU TEKRAR SORMAYINIZ!"
+    },
+    
+    serverinfo: {
+    founder: "Kurucu",
+    server_name: "Sunucu İsmi",
+    members: "Üyeler",
+    channels: "Kanallar",
+    server_boost: "Sunucu Takviyesi",
+    created: "Yaratıldı"
+    },
+    
+    userinfo: {
+    name: "İsmi",
+    sname: "Sunucudaki İsmi",
+    created: "Yaratıldı",
+    roles: "Rolleri"
+    },
+    
+    help: {
+    title: "Türkçe",
+    supportedlanguages: "Desteklenen diller:",
+    addtodiscord: "Discord'a ekle",
+    supportserver: "Destek Sunucusu",
+    voteontopgg: "Top.gg'de oy verin",
+    cstaff: "Yetkili",
+    cnsfw: "NSFW",
+    cother: "Diğer"
+    },
+    
+    ban: {
+    userreq: "Lütfen bir kullanıcı belirtin. `+ban kullanıcı sebep`",
+    usernf: "Kullanıcı bulunamadı!",
+    usernb: "Yetkilileri yasaklayamam!",
+    nreason: "Belirtilmemiş",
+    user: "Kullanıcı",
+    reason: "Sebep",
+    unban: "Yasağı Kaldır"
+    },
+    
+    unban: {
+    idreq: "Bir kullanıcı kimliği girin. `+unban kullanıcı_id`",
+    idreqq: "Bir kullanıcı kimliği girmelisiniz!",
+    usernf: "Kullanıcı bulunamadı!",
+    succ: "Kullanıcı yasağı kaldırıldı."
+    },
+    
+    perm: {
+    manage_guild: "Komutu kullanmak için `Sunucuyu Yönet` iznine sahip olmalısınız.",
+    ban_members: "Komutu kullanmak için `Üyeleri Yasakla` iznine sahip olmalısınız."
+    },
+    
+    lang: {
+    err: "Bu dil zaten ayarlanmış.",
+    msg: "Lütfen bir dil belirtin. `+lang az/en/es/tr`"
+    }
+    
+};
