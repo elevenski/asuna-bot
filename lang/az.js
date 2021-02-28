2714308fc6e40730bc1e2eeba364a64dfffdecfd
Eleven <alperiince0@gmail.com>
2021-02-28 14:13:31 +0300

    Create az.js

---
+++ lang/az.js
@@ -0,0 +1,86 @@
+module.exports = {
+    
+    embed: {
+    //
+    },
+    
+    stats: {
+    guilds: "Gildiyalar",
+    users: "İstifadəçilər",
+    channels: "Kanallar",
+    developers: "Yaradıcılar"
+    },
+    
+    nsfw: {
+    votereq: "NSFW əmrlərindən istifadə etmək üçün botu **Top.gg**'də hər 12 saatdan bir qiymətləndirməlisiniz.",
+    onreq: "NSFW Komutlarını istifadə etmək üçün kanalın NSFW xüsusiyyətini açın."    
+    },
+    
+    eightball: {
+     questionreq: "Zəhmət olmasa bir sual daxil edin. `+8ball sual`",
+     answers_one: "Bəli!",
+     answers_two: "Yox...",
+     answers_three: "Nə?",
+     answers_four: "Ciddi?",
+     answers_five: "Buna cavab vermək istəmirəm.",
+     answers_six: "Heç vaxt!",
+     answers_seven: "Yenidən cəhd elə...",
+     answers_eight: "Özünüzü inkişaf etdirməlisiniz.",
+     answers_nine: "Lütfən bu sualı təkrar verməyin!"
+    },
+    
+    serverinfo: {
+    founder: "Təsisçi",
+    server_name: "Server Adı",
+    members: "Üzvlər",
+    channels: "Kanallar",
+    server_boost: "Server Artırma",
+    created: "Yarandı"
+    },
+    
+    userinfo: {
+    name: "Ad",
+    sname: "Serverdəki Ad",
+    created: "Yarandı",
+    roles: "Rollar"
+    },
+    
+    help: {
+    title: "Azərbaycanca",
+    supportedlanguages: "Dəstəklənən dillər:",
+    addtodiscord: "Discord'a əlavə edin",
+    supportserver: "Serverə dəstək",
+    voteontopgg: "Top.gg'də səs verin",
+    cstaff: "Heyət",
+    cnsfw: "NSFW",
+    cother: "Digər"
+    }, 
+    
+    ban: {
+    userreq: "Zəhmət olmasa bir istifadəçi göstərin. `+ban istifadəçi səbəb`",
+    usernf: "İstifadəçi tapılmadı!",
+    usernb: "Səlahiyyətlilərə qadağa qoya bilmərəm!",
+    nreason: "Dəqiqləşdirilməmiş",
+    user: "İstifadəçi",
+    reason: "Səbəb",
+    unban: "Qadağanı silin"
+    },
+    
+    unban: {
+    idreq: "Zəhmət olmasa istifadəçi identifikatoru daxil edin. `+unban istifadəçi_id`",
+    idreqq: "Bir istifadəçi ID daxil etməlisiniz!",
+    usernf: "İstifadəçi tapılmadı!",
+    succ: "İstifadəçi qadağası ləğv edildi."
+    },
+    
+    perm: {
+    manage_guild: "Əmrdən istifadə etmək üçün `Serveri İdarə` etmə icazəniz olmalıdır.",
+    ban_members: "Əmrdən istifadə etmək üçün `Üzvləri qadağan edin` etmə icazəniz olmalıdır."
+    },
+    
+    lang: {
+    err: "Bu dil artıq qurulub.",
+    msg: "Zəhmət olmasa bir dil göstərin. `+lang az/en/es/tr`"
+    }
+
+};
