00a9a5a2d10e6628af4b6591ea2d7cbe9ca4ca55
Eleven <alperiince0@gmail.com>
2021-02-28 14:14:03 +0300

    Create es.js

---
+++ lang/es.js
@@ -0,0 +1,86 @@
+module.exports = {
+
+    embed: {
+    //
+    },
+    
+    stats: {
+    guilds: "Gremios",
+    users: "Usuarias",
+    channels: "Canales",
+    developers: "Desarrolladoras"
+    },
+    
+    nsfw: {
+    votereq: "Para usar los comandos NSFW, debe votar por el bot 1 en 12 horas en **Top.gg**.",
+    onreq: "Para usar los comandos NSFW, active la propiedad NSFW del canal."    
+    },
+    
+    eightball: {
+     questionreq: "Por favor ingrese una pregunta. `+8ball pregunta`",
+     answers_one: "Sí!",
+     answers_two: "No...",
+     answers_three: "Qué?",
+     answers_four: "Seriamente?",
+     answers_five: "No quiero contestar eso.",
+     answers_six: "Nunca!",
+     answers_seven: "Intentar otra vez...",
+     answers_eight: "Tienes que mejorarte a ti mismo.",
+     answers_nine: "POR FAVOR NO HAGA ESTA PREGUNTA OTRA VEZ!"
+    },
+    
+    serverinfo: {
+    founder: "Fundador",
+    server_name: "Nombre del servidor",
+    members: "Miembros",
+    channels: "Canales",
+    server_boost: "Mejora del Servidor",
+    created: "Fue creado"
+    },
+    
+    userinfo: {
+    name: "Nombre",
+    sname: "Nombre en el servidor",
+    created: "Fue creado",
+    roles: "Roles"
+    },
+    
+    help: {
+    title: "Español",
+    supportedlanguages: "Idiomas admitidos:",
+    addtodiscord: "Agregar a la discordia",
+    supportserver: "Servidor de soporte",
+    voteontopgg: "Votar en Top.gg",
+    cstaff: "Personal",
+    cnsfw: "NSFW",
+    cother: "Otra"
+    },
+    
+    ban: {
+    userreq: "Por favor especifique una usuario. `+ban usuaria razón`",
+    usernf: "Usuario no encontrado!",
+    usernb: "No puedo prohibir a las autoridades!",
+    nreason: "Sin especificar",
+    user: "Usuaria",
+    reason: "Razón",
+    unban: "Eliminar prohibición"
+    },
+    
+    unban: {
+    idreq: "Ingrese un ID de usuario. `+unban usuaria_id`",
+    idreqq: "Debes ingresar una identificación de usuario!",
+    usernf: "Usuario no encontrado!",
+    succ: "La usuario ha desbloqueado."
+    },
+    
+    perm: {
+    manage_guild: "Debe tener permiso de `Administrar servidor` para usar el comando.",
+    ban_members: "Debe tener permiso de `Prohibir miembros` para usar el comando."
+    },
+    
+    lang: {
+    err: "Este idioma ya está configurado.",
+    msg: "Especifique un idioma. `+lang az/en/es/tr`"
+    }
+    
+};
