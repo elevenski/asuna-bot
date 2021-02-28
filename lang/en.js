40204db9ab85bbcb41ed2a80ec8a1e03c585537b
Eleven <alperiince0@gmail.com>
2021-02-28 14:13:54 +0300

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,86 @@
+module.exports = {
+
+    embed: {
+        //
+    },
+    
+    stats: {
+    guilds: "Guilds",
+    users: "Users",
+    channels: "Channels",
+    developers: "Developers"
+    },
+    
+    nsfw: {
+    votereq: "To use NSFW Commands, you must vote for the bot 1 in 12 hours on **Top.gg**.",
+    onreq: "To use NSFW Commands, please turn on the NSFW property of the channel."    
+    },
+    
+    eightball: {
+     questionreq: "Please enter a question. `+8ball question`",
+     answers_one: "Yes!",
+     answers_two: "No...",
+     answers_three: "What?",
+     answers_four: "Seriously?",
+     answers_five: "I don't want to answer that.",
+     answers_six: "Never!",
+     answers_seven: "Try again...",
+     answers_eight: "You have to improve yourself.",
+     answers_nine: "PLEASE DON'T ASK THIS QUESTION AGAIN!"
+    },
+    
+    serverinfo: {
+    founder: "Founder",
+    server_name: "Server Name",
+    members: "Members",
+    channels: "Channels",
+    server_boost: "Server Boost",
+    created: "Created"
+    },
+    
+    userinfo: {
+    name: "Name",
+    sname: "Name on Server",
+    created: "Created",
+    roles: "Roles"
+    },
+    
+    help: {
+    title: "English",
+    supportedlanguages: "Supported Languages:",
+    addtodiscord: "Add to Discord",
+    supportserver: "Support Server",
+    voteontopgg: "Vote on Top.gg",
+    cstaff: "Staff",
+    cnsfw: "NSFW",
+    cother: "Other"
+    },
+    
+    ban: {
+    userreq: "Please specify a user. `+ban user reason`",
+    usernf: "User not found!",
+    usernb: "I can't ban the authorities!",
+    nreason: "Unspecified",
+    user: "User",
+    reason: "Reason",
+    unban: "Unban"
+    },
+    
+    unban: {
+    idreq: "Please enter a user ID. `+unban user_id`",
+    idreqq: "You must enter a user ID!",
+    usernf: "User not found!",
+    succ: "User has unbanned."
+    },
+    
+    perm: {
+    manage_guild: "You must have `Manage Server` permission to use the command.",
+    ban_members: "You must have `Ban Members` permission to use the command."
+    },
+    
+    lang: {
+    err: "This language is already set.",
+    msg: "Please specify a language. `+lang az/en/es/tr`"
+    }
+    
+};
